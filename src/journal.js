cdc61c12c041daebfc4fd8d4622e14251850271d
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-13 15:17:18 -0700

    Add lint

--- src/journal.js
+++ src/journal.js
@@ -1,30 +1,37 @@
 
 // business logic
-export function Entry() {
+export function Entry(title, body) {
   this.title = title;
   this.body = body;
+  this.titleBody = title + body;
 }
+
+Entry.prototype.titleBody = function () {
+  return this.body + this.title;
+};
 
 Entry.prototype.vowelCount = function() {
   var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
-  var splitBodyArray = this.body.split("");
+  var splitArray = this.titleBody.split("");
   var vowelCount = 0;
-  splitBodyArray.forEach(function(input) {
+  splitArray.forEach(function(input) {
     if (vowels.includes(input)) {
       vowelCount++;
-    }
+    } 
+    
   });
   return vowelCount;
 };
 
 Entry.prototype.consonantCount = function() {
-  var consonants = ["b", "B", "c", "C", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q", "Q", "s", "S", "t", "T", "v", "V", "w", "W", "x", "X", "y","Y", "z", "Z"];
-  var splitBodyArray = this.body.split("");
+  var consonants = ["b", "B", "c", "C", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "v", "V", "w", "W", "x", "X", "y","Y", "z", "Z"];
+  var splitArray = this.titleBody.split("");
   var consonantCount = 0;
-  splitBodyArray.forEach(function(input) {
+  splitArray.forEach(function(input) {
     if (consonants.includes(input)) {
       consonantCount++;
     }
+    
   });
   return consonantCount;
 };
--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import './styles.css';
 $(document).ready(function () {
   $("form#form1").submit(function (event) {
     event.preventDefault();
-    // var newEntry = "",
-    // var newTitle = "",
-    // var newBody = "",
-    // var journal = [];
     var newTitle = $("#title").val();
+    
     var newBody = $("#body").val();
-    var newEntry = new Entry (newTitle, newBody);
-    journal.push(newEntry);
+    
+    var newEntry = new Entry(newTitle, newBody);
+    
     var vowelcount = newEntry.vowelCount();
+    
     var consonantcount = newEntry.consonantCount();
     $("#vowelcount").text(vowelcount);
     $("#consonantcount").text(consonantcount);
